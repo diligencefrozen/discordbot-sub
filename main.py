dc2967bab1b1dbcb10a19f7deac4441218dbf976
Frozen <152419439+diligencefrozen@users.noreply.github.com>
2023-12-26 14:27:40 +0900

    Update main.py

--- main.py
+++ main.py
@@ -132,7 +132,7 @@ async def 금칙어_검사(content):
 async def on_message_edit(before, after):
     if 금칙어_검사(after.content):  # 금칙어_검사는 금칙어를 검사하는 함수입니다.
         await after.delete()
-        await after.channel.send(f"{after.author.mention},님, 수정은 우리 서버에서 금지되어 있습니다!")
+        await after.channel.send(f"{after.author.mention},님의 수정된 채팅에서 금칙어가 감지되었습니다!")
      
 @app.event
 async def on_ready():
