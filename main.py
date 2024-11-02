fb71bb981a17006466e041d74fe2ec384e0d92f8
Frozen <152419439+diligencefrozen@users.noreply.github.com>
2024-11-02 22:01:39 +0900

    Update main.py

--- main.py
+++ main.py
@@ -254,13 +254,14 @@ async def on_message(message):
         
         await message.channel.send(embed=embed)
 
-    # 파일 업로드 감지 / 2024.11.02 수정 
+    # 파일 업로드 감지 / 2024.11.02 수정  
     if message.attachments:
+        current_time = datetime.datetime.now(timezone('Asia/Seoul')).strftime('%Y-%m-%d %H:%M:%S')
         for attachment in message.attachments:
             supported_extensions = ['jpg', 'jpeg', 'png', 'gif', 'bmp', 'webp',
                                     'pdf', 'doc', 'docx', 'xls', 'xlsx', 'ppt', 'pptx', 'txt', 'hwp', 'hwpx',
                                     'mp4', 'mkv', 'mov', 'avi', 'wmv', 'flv', 'm4v', 'mp3', 'wav', 'ogg']
-
+            
             if any(attachment.filename.lower().endswith(ext) for ext in supported_extensions):
                 embed = discord.Embed(
                     title="📁 해당 기능은 Beta 버전입니다.",
@@ -268,52 +269,56 @@ async def on_message(message):
                     color=0x00ff00
                 )
                 embed.add_field(name="파일 이름", value=attachment.filename, inline=False)
-                embed.set_footer(text="오늘도 커뮤니티에 기여해주셔서 감사해요!")
-
+                embed.set_footer(text=f"개조된도리봇 | {current_time}", icon_url="https://i.imgur.com/d1Ef9W8.jpeg")
+                
                 if attachment.filename.lower().endswith(('jpg', 'jpeg', 'png', 'gif', 'bmp', 'webp')):
                     embed.set_image(url=attachment.url)
                 else:
                     embed.add_field(name="파일 다운로드", value=f"[여기 클릭]({attachment.url})", inline=False)
+                    
+                    await message.channel.send(embed=embed)
+                    return  # 중단
 
-                await message.channel.send(embed=embed)
-
-    # 성적인 키워드 감지 / 2024.11.02 수정 
+    # 성적인 키워드 감지 / 2024.11.02 수정  
     if any(pattern.search(message.content) for pattern in girl_patterns):
         dtime = datetime.datetime.now(timezone('Asia/Seoul'))
         time_str = dtime.strftime("%Y년 %m월 %d일 %H시 %M분 %S초")
-
+        current_time = dtime.strftime('%Y-%m-%d %H:%M:%S')
+        
         embed = discord.Embed(
             title="🚨 해당 기능은 Beta 버전입니다.",
             description=f"{message.author.mention} 매우 불결한 \n\n내용이 감지되었습니다.",
             color=0xff0000,
             timestamp=dtime
         )
-
+        
         embed.add_field(name="시간", value=time_str, inline=False)
         random_warning = random.choice(warning_messages)
         embed.add_field(name="경고", value=random_warning, inline=False)
-        embed.set_footer(text="반복적인 위반 시 추가적인 조치가 있을 수 있습니다.")
-
+        embed.set_footer(text=f"개조된도리봇 | {current_time}", icon_url="https://i.imgur.com/d1Ef9W8.jpeg")
         await message.channel.send(embed=embed)
+        return  # 중단
 
-    # 사용자 멘션 감지 / 2024.11.02 수정 
+    # 사용자 멘션 감지 / 2024.11.02 수정  
     if message.mentions:
+        current_time = datetime.datetime.now(timezone('Asia/Seoul')).strftime('%Y-%m-%d %H:%M:%S')
         mentioned_users = ", ".join([user.mention for user in message.mentions])
-
+        
         embed = discord.Embed(
             title="📢 해당 기능은 Beta 버전입니다.",
             description=f"{message.author.mention} 님이 \n\n{mentioned_users} 님을 호출했습니다.",
             color=0x00ff00
         )
         embed.set_image(url="https://i.imgur.com/KL3NfyD.jpeg")
-        embed.set_footer(text="개조된도리봇")
-
+        embed.set_footer(text=f"개조된도리봇 | {current_time}", icon_url="https://i.imgur.com/d1Ef9W8.jpeg")
         await message.channel.send(embed=embed)
+        return  # 중단
 
-    # 답장 감지 / 2024.11.02 수정 
+    # 답장 감지 / 2024.11.02 수정  
     if message.reference:
+        current_time = datetime.datetime.now(timezone('Asia/Seoul')).strftime('%Y-%m-%d %H:%M:%S')
         replied_message = await message.channel.fetch_message(message.reference.message_id)
-
+        
         embed = discord.Embed(
             title="💬 해당 기능은 Beta 버전입니다.",
             description=f"{message.author.mention} 님이 {replied_message.author.mention} 님의 \n\n메시지에 답장을 달았습니다.",
@@ -321,12 +326,13 @@ async def on_message(message):
         )
         embed.add_field(name="답장 내용", value=message.content, inline=False)
         embed.add_field(name="원본 메시지", value=replied_message.content, inline=False)
-        embed.set_footer(text="개조된도리봇")
-
+        embed.set_footer(text=f"개조된도리봇 | {current_time}", icon_url="https://i.imgur.com/d1Ef9W8.jpeg")
         await message.channel.send(embed=embed)
+        return  # 중단
 
-    # 영어 채팅 감지  / 2024.11.02 수정 
+    # 영어 채팅 감지  / 2024.11.02 수정  
     if re.search(r'[a-zA-Z]', message.content):
+        current_time = datetime.datetime.now(seoul_tz).strftime('%Y-%m-%d %H:%M:%S')
         embed = discord.Embed(
             title="📢 해당 기능은 Beta 버전입니다.",
             description=f"{message.author.mention} 님이 영어로 \n\n채팅을 시도했습니다.",
@@ -334,9 +340,9 @@ async def on_message(message):
         )
         embed.add_field(name="메시지 내용", value=message.content, inline=False)
         embed.set_image(url="https://i.imgur.com/XgrhOwC.jpeg")
-        embed.set_footer(text="개조된도리봇")
-
+        embed.set_footer(text=f"개조된도리봇 | {current_time}", icon_url="https://i.imgur.com/d1Ef9W8.jpeg")
         await message.channel.send(embed=embed)
+        return  # 중단
 
     # 역사 명령어 처리 / 2024.09.14 수정 
     if message.content.startswith("=역사"):
